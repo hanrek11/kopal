5e14aaa078a90625e6b8626afcf569bca04c76c4
hanrek11 <132694523+hanrek11@users.noreply.github.com>
2023-05-05 13:39:50 -0300

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,3 @@
-const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=testtoken=https://test.link.com&html_redirect=1 ";
+const offerLink = "vnd.youtube://youtube.com/redirect?event=comments&redir_token=QUFFLUhqbllNc05WVncyeE1YdWo1dUxzbXZETF8walFnQXxBQ3Jtc0tsUzZzY2dEb1lCY3Ywd3U2SEtqTGRnVU9qc0tldkl0NFNhYjNWdGl5OHNCcjVfUno0SGo4SXAxMGVLbks5MEdhQWlpSVdvYS1rR1VQWXRmM3c4Qm1MTVA1emF1bUxDWjFjVG9fLVg3ZTZUZ245RWQxcw&q=https%3A%2F%2Fcuty.io%2FcA0IT5&html_redirect=1
+
+ ";
